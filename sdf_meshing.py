0b89d78bf4bc2e39fd9ec1c39315c022dbf72359
comar <saywooong@gmail.com>
2021-12-14 14:07:02 +0900

    feat: test for sdf with rgb

--- sdf_meshing.py
+++ sdf_meshing.py
@@ -87,7 +87,7 @@ def create_mesh_rgb_multi(
         voxel_size = 2.0 / (N - 1)
 
         overall_index = torch.arange(0, N ** 3, 1, out=torch.LongTensor())
-        samples = torch.zeros(N ** 3, 4)
+        samples = torch.zeros(N ** 3, 7)
 
         # transform first 3 columns
         # to be the x, y, z index
@@ -117,23 +117,36 @@ def create_mesh_rgb_multi(
             embd = embd.cuda()
             
             model_input = {'coords': sample_subset, 'ids': embd}
+            _, sdf, rgb = decoder(model_input)
 
             samples[head : min(head + max_batch, num_samples), 3] = (
-                decoder(model_input)['model_out']
+                sdf
+                .squeeze()#.squeeze(1)
+                .detach()
+                .cpu()
+            )
+
+            samples[head : min(head + max_batch, num_samples), 4:] = (
+                rgb 
                 .squeeze()#.squeeze(1)
                 .detach()
                 .cpu()
             )
             head += max_batch
+            break;
 
         sdf_values = samples[:, 3]
         sdf_values = sdf_values.reshape(N, N, N)
 
+        rgb_values = samples[:, 4:]
+        rgb_values = rgb_values.reshape(N, N, N, -1)
+
         end = time.time()
         print("sampling takes: %f" % (end - start))
 
-        convert_sdf_samples_to_ply(
+        convert_sdfc_samples_to_ply(
             sdf_values.data.cpu(),
+            rgb_values.data.cpu(),
             voxel_origin,
             voxel_size,
             ply_filename + str(code) + ".ply",
@@ -271,6 +284,80 @@ def create_mesh(
         scale,
     )
 
+
+def convert_sdfc_samples_to_ply(
+    pytorch_3d_sdf_tensor,
+    pytorch_3d_rgb_tensor,
+    voxel_grid_origin,
+    voxel_size,
+    ply_filename_out,
+    offset=None,
+    scale=None,
+):
+    """
+    Convert sdf samples to .ply
+
+    :param pytorch_3d_sdf_tensor: a torch.FloatTensor of shape (n,n,n)
+    :voxel_grid_origin: a list of three floats: the bottom, left, down origin of the voxel grid
+    :voxel_size: float, the size of the voxels
+    :ply_filename_out: string, path of the filename to save to
+
+    This function adapted from: https://github.com/RobotLocomotion/spartan
+    """
+
+    start_time = time.time()
+
+    numpy_3d_sdf_tensor = pytorch_3d_sdf_tensor.numpy()
+    numpy_3d_rgb_tensor = pytorch_3d_rgb_tensor.numpy()
+
+    verts, faces, normals, values = np.zeros((0, 3)), np.zeros((0, 3)), np.zeros((0, 3)), np.zeros(0)
+    try:
+        verts, faces, normals, values = skimage.measure.marching_cubes_lewiner(
+            numpy_3d_sdf_tensor, level=0.0, spacing=[voxel_size] * 3
+        )
+    except:
+        pass
+
+    # transform from voxel coordinates to camera coordinates
+    # note x and y are flipped in the output of marching_cubes
+    mesh_points = np.zeros_like(verts)
+    mesh_points[:, 0] = voxel_grid_origin[0] + verts[:, 0]
+    mesh_points[:, 1] = voxel_grid_origin[1] + verts[:, 1]
+    mesh_points[:, 2] = voxel_grid_origin[2] + verts[:, 2]
+
+    # apply additional offset and scale
+    if scale is not None:
+        mesh_points = mesh_points / scale
+    if offset is not None:
+        mesh_points = mesh_points - offset
+
+    # try writing to the ply file
+
+    num_verts = verts.shape[0]
+    num_faces = faces.shape[0]
+
+    verts_tuple = np.zeros((num_verts,), dtype=[("x", "f4"), ("y", "f4"), ("z", "f4")])
+
+    for i in range(0, num_verts):
+        verts_tuple[i] = tuple(mesh_points[i, :])
+
+    faces_building = []
+    for i in range(0, num_faces):
+        faces_building.append(((faces[i, :].tolist(),)))
+    faces_tuple = np.array(faces_building, dtype=[("vertex_indices", "i4", (3,))])
+
+    el_verts = plyfile.PlyElement.describe(verts_tuple, "vertex")
+    el_faces = plyfile.PlyElement.describe(faces_tuple, "face")
+
+    ply_data = plyfile.PlyData([el_verts, el_faces])
+    logging.debug("saving mesh to %s" % (ply_filename_out))
+    ply_data.write(ply_filename_out)
+
+    logging.debug(
+        "converting to ply format and writing to file took {} s".format(
+            time.time() - start_time
+        )
+    )
 
 def convert_sdf_samples_to_ply(
     pytorch_3d_sdf_tensor,
